2d02acbefe8f290a12a9e0cdc62dbab6e2695921
Gustavo RPS <516827+gustavorps@users.noreply.github.com>
2020-06-29 14:04:28 -0300

    Update vars.ts
    Use connection URI to make the application more DB agnostic, sequelize call as _connection string_:
    - https://www.postgresql.org/docs/12/libpq-connect.html#id-1.7.3.8.3.6
    - https://sequelize.org/v3/docs/usage/

--- src/config/vars.ts
+++ src/config/vars.ts
@@ -4,11 +4,7 @@ const variables: any = {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
   logs: process.env.NODE_ENV === 'production' ? 'combined' : 'dev',
-  postgresUser: process.env.POSTGRES_USER,
-  postgresPassword: process.env.POSTGRES_PASSWORD,
-  postgresDataBase: process.env.POSTGRES_DATA_BASE,
-  postgresHost: process.env.POSTGRES_HOST,
-  postgresDialect: process.env.POSTGRES_DIALECT
+  dbConnectionURI:  process.env.DB_CONNECTION_URI || 'postgresql://localhost:5433'
 }
 
 export { variables }
